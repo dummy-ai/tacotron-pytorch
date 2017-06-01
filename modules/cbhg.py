f7d29f3e19f6d7da04c903823c69eaf00cb0ed56
Hezheng Yin <yinhezheng1995@gmail.com>
2017-06-01 10:28:52 -0700

    Fix various bugs in CBHG module to pass unit test

--- modules/cbhg.py
+++ modules/cbhg.py
@@ -1,21 +1,33 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.autograd import Variable
 from modules.conv1d import Conv1dBankWithMaxPool, Conv1dProjection
 from modules.highway import HighwayNet
 
 class CBHG(nn.Module):
 
-    def __init__(self, bank_k, bank_ck, proj_dims):
+    def __init__(self, bank_k, bank_ck, proj_dims, 
+        highway_layers, highway_units, gru_units, gru_layers=1):
         super(CBHG, self).__init__()
 
         self._bank_k = bank_k
         self._bank_ck = bank_ck
         self._proj_dims = proj_dims
+        self._highway_layers = highway_layers
+        self._highway_units = highway_units
+        self._gru_units = gru_units
+        self._gru_layers = gru_layers
 
-        self.convbank = Conv1dBankWithMaxPool(self.bank_k, self.bank_ck)
+        self.convbank = Conv1dBankWithMaxPool(bank_k, 
+                                              bank_ck)
         self.convproj = Conv1dProjection(proj_dims)
-        self.highway = HighwayNet(4)
+        self.highway = HighwayNet(highway_layers, 
+                                  highway_units)
+
+        self.gru = nn.GRU(highway_units, gru_units, gru_layers, 
+                          batch_first=True,
+                          bidirectional=True)
 
     @property
     def bank_k(self):
@@ -29,20 +41,46 @@ class CBHG(nn.Module):
     def proj_dims(self):
         return self._proj_dims
 
+    @property
+    def highway_layers(self):
+        return self._highway_layers
+
+    @property
+    def highway_units(self):
+        return self._highway_units
+
+    @property
+    def gru_units(self):
+        return self._gru_units
+
+    @property
+    def gru_layers(self):
+        return self._gru_layers
+
     def forward(self, x):
-        x = torch.transpose(x, 1, 2)
+        """
+        Args:
+            x: A Tensor of size (batch_size,
+                                 time_steps,
+                                 in_channels)
+
+        Returns:
+            A Tensor of size (batch_size,
+                              time_steps,
+                              2 * gru_units)
+        """
+        x = torch.transpose(x, 1, 2).contiguous()
         bank_out = self.convbank(x)
         proj_out = self.convproj(x)
         res_out = x + proj_out
         highway_out = self.highway(res_out)
 
-        hidden_size = 128
-        self.rnn = nn.GRU(128, hidden_size, 1, 
-                          batch_first=True,
-                          bidirectional=True)
-        rnn_inputs = torch.transpose(highway_out, 1, 2)
+        # rnn_inputs has size (batch_size, time_steps, highway_units)
+        rnn_inputs = torch.transpose(highway_out, 1, 2).contiguous()
 
         batch_size = x.size()[0]
-        h0 = Variable(torch.randn(2, batch_size, hidden_size))
-        final_output, _ = self.rnn(rnn_inputs, h0)
+        h0 = Variable(torch.randn(self.gru_layers * 2, 
+                                  batch_size, 
+                                  self.gru_units))
+        final_output, _ = self.gru(rnn_inputs, h0)
         return final_output
