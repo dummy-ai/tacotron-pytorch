e64f66105bde6cbbf732e8195bbb27818dd9067b
Hezheng Yin <yinhezheng1995@gmail.com>
2017-06-19 00:01:57 -0700

    Add unit test for pre-net

---
+++ test/test_prenet.py
@@ -0,0 +1,32 @@
+from modules.prenet import *
+from torch.autograd import Variable
+
+def test_prenet():
+	fc1_hidden_size = 256
+	fc2_hidden_size = 128
+
+	# simulate pre-net in decoder
+	batch_size = 32
+	input_size = 80
+	input = Variable(torch.randn(batch_size, 1, input_size))
+
+	prenet = PreNet(input_size, 
+					fc1_hidden_size=fc1_hidden_size, 
+					fc2_hidden_size=fc2_hidden_size)
+	output = prenet(input)
+
+	assert output.size() == (batch_size, 1, fc2_hidden_size)
+
+	# simulate pre-net in encoder
+	batch_size = 32
+	embedding_size = 256
+	time_steps = 17
+	input2 = Variable(torch.randn(batch_size, time_steps, embedding_size))
+
+	prenet2 = PreNet(embedding_size,
+					 fc1_hidden_size=fc1_hidden_size,
+					 fc2_hidden_size=fc2_hidden_size)
+
+	output2 = prenet2(input2)
+
+	assert output2.size() == (batch_size, time_steps, fc2_hidden_size)
